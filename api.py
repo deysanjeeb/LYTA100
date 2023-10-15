5850c17241f4188863338a8046b2bfb81e9e506c
Sanjeeb Dey <sanjeebdey1@gmail.com>
2023-10-14 20:19:57 -0700

    Created palm api
    Signed-off-by: Sanjeeb Dey <sanjeebdey1@gmail.com>

--- api.py
+++ api.py
@@ -9,6 +9,30 @@ import google.generativeai as palm
 import config
 
 palm_api_key = config.API_KEYS['palm']
+palm.configure(api_key=palm_api_key)
+defaults = {
+  'model': 'models/chat-bison-001',
+  'temperature': 0.25,
+  'candidate_count': 1,
+  'top_k': 40,
+  'top_p': 0.95,
+}
+context = ""
+examples = [
+  [
+    "Hey. I am going to describe a scene or place I'm imagining. And I want you to help describe what it might feel like to be in that scene.",
+    "Great! I will help you imagine your scene. I'll give you vivid, amazing descriptions what it would feel like to be there! I'll also provide follow-up suggestion so you can ask for more details."
+  ],
+  [
+    "Imagine I'm at an underwater jazz performance. Describe what it looks, feels, and sounds like!",
+    "This underwater jazz performance is so epic! There are thousands of sea creatures all around jamming. A sting ray is carrying the melody on a trombone. And whoaa, that sea turtle is really rocking out on the drums! You feel the rhythm of jazz is your bones! // Follow-up: You can ask me \"what's the name of their band?\" or \"is that a whale playing upright bass?\" or \"what song are they playing?\""
+  ],
+  [
+    "Imagine I'm at a moon of Jupiter. Describe what it looks, feels, and sounds like!",
+    "You're on Ganymede, a moon of Jupiter! You're being bombarded by some kind of plasma rain! It feels tingly and weird. Ooh, there seems to be a liquid ocean sloshing beneath your feet! You see Jupiter, with its glorious, mesmerizing blend of orange in the distance. // Follow-up: You can ask me \"what does it smell like?\" or \"Can I see other moons or planets?\" or \"what is the strange light effect all around me?\""
+  ]
+]
+messages = []
 
 app = Flask(__name__)
 
@@ -61,7 +85,17 @@ def chat():
     if 'text' not in request.form:
         return jsonify({'error': 'No text provided'})
     
-    text = request.form['text']
+    chatMessage = request.form['text']
+    messages.append(chatMessage)
+    response = palm.chat(
+        **defaults,
+        context=context,
+        examples=examples,
+        messages=messages
+    )
+    print(response.last) # Response of the AI to your most recent request
+    return jsonify({'response': response.last})
+
 
    
 
