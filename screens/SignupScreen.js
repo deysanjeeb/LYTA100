d814192a2ab60be390c46cf5f15045256fe0a7db
Divija Naredla <divija.naredla@sjsu.edu>
2023-10-15 07:47:42 -0700

    Styling changes

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,36 +1,93 @@
 // screens/SignupScreen.js
-import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignup = () => {
-    // Add signup logic here
-    console.log("Signup:", email, password);
-  };
-
+const SignupScreen = () => {
   return (
-    <View>
-      <Text>Signup Screen</Text>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Signup" onPress={handleSignup} />
-      <Text onPress={() => navigation.navigate("Login")}>
-        Already have an account? Log in!
-      </Text>
-    </View>
+    <LinearGradient colors={["#3498db", "#2980b9"]} style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput style={styles.input} placeholder="Full Name" />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.signupButton}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        <View style={styles.loginContainer}>
+          <Text style={styles.loginText}>Already have an account?</Text>
+          <TouchableOpacity>
+            <Text style={styles.loginLink}>Login here</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerContainer: {
+    width: "80%",
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.8)", // Semi-transparent white background
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#2c3e50", // Dark blue color
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+  },
+  signupButton: {
+    backgroundColor: "#27ae60", // Green color
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  loginContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  loginText: {
+    color: "#34495e", // Dark gray color
+  },
+  loginLink: {
+    color: "#3498db", // Blue color
+    marginLeft: 5,
+  },
+});
 
 export default SignupScreen;
